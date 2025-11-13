36bc7715b31778aaae6d738194073aaf3c929c3d
Manish Kumar <manishgupta3859@gmail.com>
2025-11-13 11:17:46 +0530

    dashboard-authentication

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,33 +1,49 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+  import React from "react";
+  import { Route, Routes } from "react-router-dom";
 
-import Apps from "./Apps";
-import Funds from "./Funds";
-import Holdings from "./Holdings";
+  import Apps from "./Apps";
+  import Funds from "./Funds";
+  import Holdings from "./Holdings";
 
-import Orders from "./Orders";
-import Positions from "./Positions";
-import Summary from "./Summary";
-import WatchList from "./WatchList";
-import { GeneralContextProvider } from "./GeneralContext";
-const Dashboard = () => {
-  return (
-    <div className="dashboard-container">
-      <GeneralContextProvider>
-        <WatchList />
-      </GeneralContextProvider>
-      <div className="content">
-        <Routes>
-          <Route exact path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
-        </Routes>
+  import Orders from "./Orders";
+  import Positions from "./Positions";
+  import Summary from "./Summary";
+  import WatchList from "./WatchList";
+  import { GeneralContextProvider } from "./GeneralContext";
+  import { useEffect } from "react";
+  import axios from"axios"
+  const Dashboard = () => {
+    useEffect(()=>{
+      axios.post("https://zerodha-backend-pho2.onrender.com/auth/",
+        {},
+        {withCredentials:true},
+      ).then((res)=>{
+        if(!res.data.status){
+            window.location.href =" /login";
+        }
+      }).catch((err)=>{
+        console.log("Auth checked failed ",err);
+        window.location.href = "/login";
+      })
+    },[]);
+
+    return (
+      <div className="dashboard-container">
+        <GeneralContextProvider>
+          <WatchList />
+        </GeneralContextProvider>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Summary />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/holdings" element={<Holdings />} />
+            <Route path="/positions" element={<Positions />} />
+            <Route path="/funds" element={<Funds />} />
+            <Route path="/apps" element={<Apps />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Dashboard;
+  export default Dashboard;
